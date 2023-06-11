7d0a94765c095f4ff06d52c21660a447af31b539
jyoung7988 <jyoung7988@naver.com>
2023-06-11 21:24:59 +0900

    feat : 파라미터 넘기지 않고 tracId 동기화 앱 적용

---
+++ src/main/java/jiny/advanced/app/v3/OrderServiceV3.java
@@ -0,0 +1,28 @@
+package jiny.advanced.app.v3;
+
+import jiny.advanced.trace.TraceId;
+import jiny.advanced.trace.TraceStatus;
+import jiny.advanced.trace.hellotrace.HelloTraceV2;
+import jiny.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV3 {
+
+    private final OrderRepositoryV3 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(TraceId traceId, String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
