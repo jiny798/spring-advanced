8737542cd5c3a11cb7ec7b126bd93f67831e209a
jyoung7988 <jyoung7988@naver.com>
2023-08-05 19:13:11 +0900

    add : 템플릿 메서드 패턴 - 적용1

---
+++ src/main/java/jiny/advanced/app/v4/OrderServiceV4.java
@@ -0,0 +1,27 @@
+package jiny.advanced.app.v4;
+
+import jiny.advanced.trace.TraceStatus;
+import jiny.advanced.trace.logtrace.LogTrace;
+import jiny.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV4 {
+
+    private final OrderRepositoryV4 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+
+        AbstractTemplate<Void> template = new AbstractTemplate<Void>(trace) {
+            @Override
+            protected Void call() {
+                orderRepository.save(itemId);
+                return null;
+            }
+        };
+        template.execute("OrderService.orderItem()");
+    }
+}
