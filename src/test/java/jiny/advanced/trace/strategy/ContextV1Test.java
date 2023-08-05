39f491715224a454c74683f70e857dffdc1715c0
jyoung7988 <jyoung7988@naver.com>
2023-08-06 02:34:00 +0900

    test : 전략패턴 예제2 - 익명 내부 클래스를 변수에 담지않고, 생성하면서 바로 컨텍스트에 전달 및 람다 사용

--- src/test/java/jiny/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/jiny/advanced/trace/strategy/ContextV1Test.java
@@ -68,4 +68,30 @@ public class ContextV1Test {
         ContextV1 context2 = new ContextV1(strategyLogic2);
         context2.execute();
     }
+
+    @Test
+    void strategyV3() {
+        ContextV1 context1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context1.execute();
+        ContextV1 context2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        context2.execute();
+    }
+
+    @Test
+    void strategyV4() {
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        context1.execute();
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        context2.execute();
+    }
 }
