003cda7f41c83d50f2d92fcac12b6d7679eb5320
jyoung7988 <jyoung7988@naver.com>
2023-08-06 17:43:40 +0900

    add : 프록시패턴 예제 코드2 - 프록시 객체 사용

--- src/test/java/jiny/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/jiny/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,7 +1,9 @@
 package jiny.proxy.pureproxy.proxy;
 
+import jiny.proxy.pureproxy.proxy.code.CacheProxy;
 import jiny.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import jiny.proxy.pureproxy.proxy.code.RealSubject;
+import jiny.proxy.pureproxy.proxy.code.Subject;
 import org.junit.jupiter.api.Test;
 
 public class ProxyPatternTest {
@@ -9,6 +11,16 @@ public class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        Subject realSubject = new RealSubject();
+        Subject cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
