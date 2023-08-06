e5057ad46de4b7ea1094191eec8d98d9dd102c88
jyoung7988 <jyoung7988@naver.com>
2023-08-06 22:23:25 +0900

    feat : 원본 코드 수정하지 않고 로그 추적기 적용 (v1) - 인터페이스가 있는 구현 클래스에 적용

--- src/main/java/jiny/proxy/ProxyApplication.java
+++ src/main/java/jiny/proxy/ProxyApplication.java
@@ -1,15 +1,24 @@
 package jiny.proxy;
 
+import jiny.advanced.trace.logtrace.LogTrace;
+import jiny.advanced.trace.logtrace.ThreadLocalLogTrace;
 import jiny.proxy.config.AppV1Config;
 import jiny.proxy.config.AppV2Config;
+import jiny.proxy.config.v1_proxy.InterfaceProxyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({AppV1Config.class, AppV2Config.class})
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "jiny.proxy.app") //주의
 public class ProxyApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProxyApplication.class, args);
+    }
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
     }
 }
