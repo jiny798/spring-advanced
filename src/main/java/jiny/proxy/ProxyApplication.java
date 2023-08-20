d7cb5857828bff096e7a8b15cdc777a1620f667c
jyoung7988 <jyoung7988@naver.com>
2023-08-20 20:11:55 +0900

    feat: JDK동적 프록시 적용 1

--- src/main/java/jiny/proxy/ProxyApplication.java
+++ src/main/java/jiny/proxy/ProxyApplication.java
@@ -6,6 +6,7 @@ import jiny.proxy.config.AppV1Config;
 import jiny.proxy.config.AppV2Config;
 import jiny.proxy.config.v1_proxy.ConcreteProxyConfig;
 import jiny.proxy.config.v1_proxy.InterfaceProxyConfig;
+import jiny.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +14,8 @@ import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class) //구체 클래스 기반 프록시
+//@Import(ConcreteProxyConfig.class) //구체 클래스 기반 프록시
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "jiny.proxy.app") //주의
 public class ProxyApplication {
     public static void main(String[] args) {
